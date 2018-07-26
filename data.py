dc90888249fdf7034afa26d4d1e266f39ced5908
LittleTodd <ljungberg@gmail.com>
2018-07-26 16:50:16 +0800

    Initial commit

---
+++ data.py
@@ -0,0 +1,16 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article Two'
+        },
+        {
+            'id' : 2,
+            'title' : 'Article Two'
+        },
+        {
+            'id' : 3,
+            'title' : 'Article Three'
+        }
+    ]
+    return articles
